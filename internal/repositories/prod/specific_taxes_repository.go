8b3d101b9b32a51cd8ebf6a3a9ddf4ac45ea5c72
Ärsen <arsen.serikkaliev.04@mail.ru>
2024-08-27 19:22:28 +0500

    push

--- internal/repositories/prod/specific_taxes_repository.go
+++ internal/repositories/prod/specific_taxes_repository.go
@@ -32,11 +32,15 @@ func (r *SpecificTaxesRepository) GetSpecificTaxes(ctx context.Context, year int
 	result := make(map[string]float64)
 	for rows.Next() {
 		var nameShortRu string
-		var totalSpecificTax float64
+		var totalSpecificTax sql.NullFloat64 // Use sql.NullFloat64 to handle potential NULLs
 		if err := rows.Scan(&nameShortRu, &totalSpecificTax); err != nil {
 			return nil, err
 		}
-		result[nameShortRu] = totalSpecificTax
+		if totalSpecificTax.Valid { // Check if the value is valid
+			result[nameShortRu] = totalSpecificTax.Float64
+		} else {
+			result[nameShortRu] = 0.0 // Handle NULLs as 0.0
+		}
 	}
 
 	if err := rows.Err(); err != nil {
